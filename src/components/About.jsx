2bd4d4d67c848fac8af49b4754b87132c355e0b3
othmane <othmanesmouni08@gmzil.com>
2026-01-28 10:51:14 +0100

    Personal portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <section id="about" className="section about">
+      <div className="about-wrapper">
+        <div className="about-photo">
+          <img src="/docs/OTHMANE.jpg" alt="Othmane Smouni" />
+        </div>
+        <div className="about-text">
+          <h2>About Me</h2>
+          <p>
+            Hello! I’m Othmane, a Junior Full Stack MERN Developer with a strong
+            foundation in building modern and scalable web applications.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
